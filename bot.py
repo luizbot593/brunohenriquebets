7704ae89d85b0678606b74830cac1faa6c5c8cda
luizbot593 <lfmoliveira7@gmail.com>
2025-06-01 14:50:11 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,3 +47,18 @@ def liberar_usuario(message):
 
 # Rodar o bot
 bot.polling()
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot online!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
